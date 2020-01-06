1faacfa7012b8875d211cb2cd94571e7e5c0dfc1
paulasjes-stripe <paulasjes@stripe.com>
2020-01-06 14:41:07 +0800

    wtf git why can't you deal with caps

--- types/webhooks.d.ts
+++ types/webhooks.d.ts
@@ -1,53 +1,87 @@
-declare namespace Stripe {
-  export class Webhooks {
-    /**
-     * Constructs and verifies the signature of an Event from the provided details.
-     *
-     * @param {string} payload    Raw text body payload received from Stripe
-     * @param {string} header     Value of the `stripe-signature` header from Stripe
-     * @param {string} secret     Your Webhook Signing Secret
-     * @param {number} tolerance  Seconds of tolerance on timestamps
-     *
-     * @returns Stripe.Event
-     *
-     * @throws Error.StripeSignatureVerificationError
-     */
-    constructEvent(payload: string, header :string, secret: string, tolerance?: number): Stripe.Event;
+/// <reference types="node" />
 
-    /**
-     * Generates a header to be used for webhook mocking
-     *
-     * @typedef {object} opts
-     * @param {string} payload - JSON stringified payload object, containing the 'id' and 'object' parameters
-     * @param {number} timestamp - Timestamp of the header. Defaults to Date.now()
-     * @param {string} secret - Stripe webhook secret 'whsec_...'
-     * @param {string} scheme - Version of API to hit. Defaults to 'v1'.
-     * @param {string} signature - Computed webhook signature
-     */
-    generateTestHeaderString(opts: {
-      timestamp: number
-      payload: string
-      secret: string
-      scheme?: string
-      signature: string
-    }) : string;
+declare module 'stripe' {
+  namespace Stripe {
+    export class Webhooks {
+      /**
+       * Constructs and verifies the signature of an Event from the provided details.
+       *
+       * @throws Stripe.errors.StripeSignatureVerificationError
+       */
+      constructEvent(
+        /**
+         * Raw text body payload received from Stripe.
+         */
+        payload: string | Buffer,
 
-    signature: Signature;
-  }
+        /**
+         * Value of the `stripe-signature` header from Stripe.
+         */
+        header: string,
 
-  namespace Error {
-    interface StripeSignatureVerificationError {}
-  }
+        /**
+         * Your Webhook Signing Secret for this endpoint (e.g., 'whsec_...').
+         * You can get this [in your dashboard](https://dashboard.stripe.com/webhooks).
+         */
+        secret: string,
 
-  export class Signature {
-    EXPECTED_SCHEME: 'v1'
+        /**
+         * Seconds of tolerance on timestamps.
+         */
+        tolerance?: number
+      ): Stripe.Event;
 
-    _computeSignature(payload: string, secret: string): string;
-    verifyHeader(payload:string , header:string, secret:string, tolerance?:number):void;
-    parseHeader(header:string, scheme?: string): {
-      t: number
-      v0: string
-      v1: string
-    };
+      /**
+       * Generates a header to be used for webhook mocking
+       */
+      generateTestHeaderString(opts: {
+        /**
+         * JSON stringified payload object, containing the 'id' and 'object' parameters.
+         */
+        payload: string;
+
+        /**
+         * Timestamp of the header. Defaults to Date.now().
+         */
+        timestamp?: number;
+
+        /**
+         * Stripe webhook secret, e.g., 'whsec_...'.
+         */
+        secret: string;
+
+        /**
+         * Version of API to hit. Defaults to 'v1'.
+         */
+        scheme?: string;
+
+        /**
+         * Computed webhook signature.
+         */
+        signature?: string;
+      }): string;
+
+      signature: Signature;
+    }
+
+    export class Signature {
+      EXPECTED_SCHEME: 'v1';
+
+      _computeSignature(payload: string, secret: string): string;
+      verifyHeader(
+        payload: string,
+        header: string,
+        secret: string,
+        tolerance?: number
+      ): void;
+      parseHeader(
+        header: string,
+        scheme?: string
+      ): {
+        t: number;
+        v0: string;
+        v1: string;
+      };
+    }
   }
 }
